69f6e37089b44ccd1ba06f51ea70bff9e7ee33b1
Bumpyy <fsghassan2429d@gmail.com>
2026-01-05 14:42:44 +0700

    update z-index

--- resources/views/pages/tictactivity.blade.php
+++ resources/views/pages/tictactivity.blade.php
@@ -7,7 +7,7 @@
     <div
         class="before:bg-size-[70%_auto] before:bg-bottom-right relative before:absolute before:left-0 before:top-0 before:-z-10 before:inline-block before:h-full before:w-full before:bg-[url('../assets/bg/tictactivity-bg-island.png')] before:bg-no-repeat">
 
-        <div class="absolute left-[1%] top-[7%] w-[13vw] max-md:hidden">
+        <div class="absolute left-[1%] top-[7%] z-0 w-[13vw] max-md:hidden">
             <div class="relative h-full w-full">
                 <img class="cloud absolute h-auto w-auto" data-direction="right" data-speed="1"
                     src="{{ asset('img/cloud.png') }}" alt="" style="width: 290px; height: auto;">
