9c4406ed1e240ca878da04cde447bc9d08e4aeed
julians22 <dabnerjulian@yahoo.com>
2026-08-20 16:29:09 +0700

    add path

--- resources/views/components/pop-up.blade.php
+++ resources/views/components/pop-up.blade.php
@@ -1,5 +1,5 @@
 <x-modal model="openPopUp">
-    <div class="flex items-center justify-center w-full h-full p-4">
+    <div class="flex justify-center items-center p-4 w-full h-full">
         <div @class([
             'relative',
             'slider-outer-shadow rounded-4xl bg-card-blue p-2 md:p-3' => db_config('pop-up.enable_container'),
@@ -14,22 +14,22 @@
                     @endif
                     <picture>
                         <source
-                            srcset="{{ asset('storage/' . pathinfo(db_config('pop-up.image'), PATHINFO_FILENAME) . '-mobile.webp') }}"
+                            srcset="{{ asset('storage/' . pathinfo(db_config('pop-up.image'), PATHINFO_DIRNAME) . '/' . pathinfo(db_config('pop-up.image'), PATHINFO_FILENAME) . '-mobile.webp') }}"
                             media="(max-width: 767px)"
                             type="image/webp"
                         >
                         <source
-                            srcset="{{ asset('storage/' . pathinfo(db_config('pop-up.image'), PATHINFO_FILENAME) . '-mobile.' . pathinfo(db_config('pop-up.image'), PATHINFO_EXTENSION)) }}"
+                            srcset="{{ asset('storage/' . pathinfo(db_config('pop-up.image'), PATHINFO_DIRNAME) . '/' . pathinfo(db_config('pop-up.image'), PATHINFO_FILENAME) . '-mobile.' . pathinfo(db_config('pop-up.image'), PATHINFO_EXTENSION)) }}"
                             media="(max-width: 767px)"
                         >
                         <source
-                            srcset="{{ asset('storage/' . pathinfo(db_config('pop-up.image'), PATHINFO_FILENAME) . '.webp') }}"
+                            srcset="{{ asset('storage/' . pathinfo(db_config('pop-up.image'), PATHINFO_DIRNAME) . '/' . pathinfo(db_config('pop-up.image'), PATHINFO_FILENAME) . '.webp') }}"
                             type="image/webp"
                         >
                         <img
-                            src="{{ asset('storage/' . db_config('pop-up.image')) }}"
+                            src="{{ asset('storage/' . pathinfo(db_config('pop-up.image'), PATHINFO_DIRNAME) . '/' . pathinfo(db_config('pop-up.image'), PATHINFO_BASENAME)) }}"
                             alt=""
-                            class="block mx-auto w-auto h-auto max-w-[90vw] md:max-w-[80vw] lg:max-w-[900px] max-h-[75vh] md:max-h-[85vh] object-contain rounded-xl"
+                            class="block mx-auto rounded-xl w-auto max-w-[90vw] md:max-w-[80vw] lg:max-w-[900px] h-auto max-h-[75vh] md:max-h-[85vh] object-contain"
                         >
                     </picture>
                     @if (db_config('pop-up.url'))
@@ -40,11 +40,11 @@
             <button
                 type="button"
                 @click="openPopUp = false"
-                class="absolute -top-4 -right-4 md:-top-6 md:-right-6 z-50"
+                class="-top-4 md:-top-6 -right-4 md:-right-6 z-50 absolute"
             >
                 <img
                     src="{{ asset('img/close-icon.png') }}"
-                    class="w-10 h-10 md:w-14 md:h-14"
+                    class="w-10 md:w-14 h-10 md:h-14"
                     alt="Close"
                 >
             </button>
